e745405471c3c20997a86f9be28346f62bd089fb
goodsungeun <goodsungeun@localhost>
2021-05-30 20:54:16 +0900

    Modified UsingBufferedReader.java

--- src/main/java/edu/handong/csee/java/example/fileread/benchmark/UsingBufferedReader.java
+++ src/main/java/edu/handong/csee/java/example/fileread/benchmark/UsingBufferedReader.java
@@ -13,7 +13,7 @@ public class UsingBufferedReader {
 
         StopWatch.start();
 
-        BufferedReader inputStream= new BufferedReader(new FileReader(DumpDataWriter.input1MB));
+        BufferedReader inputStream= new BufferedReader(new FileReader(args[0]));
         while (inputStream.read()!=-1){}
 
         long duration = StopWatch.stop();
@@ -23,6 +23,8 @@ public class UsingBufferedReader {
 
 
         //-------------- Test reading 10 MB file. --------------------
+        
+        /*
 
         StopWatch.start();
 
@@ -33,6 +35,7 @@ public class UsingBufferedReader {
         System.out.println(duration2 + " milsec");
 
         inputStream2.close();
+        */
 
         /*
         //-------------- Test reading 100 MB file. --------------------
